4e65aac472a76191bf445deeff6195a7939cfb59
LongSamma <longphamk2@gmail.com>
2025-03-08 21:52:43 +0700

    'update'

---
+++ JSCODE/Notes.js
@@ -0,0 +1,17 @@
+
+
+function gow3school() {
+    window.location.href = "https://www.w3schools.com/";
+}
+
+function goleetcode() {
+    window.location.href = "https://leetcode.com/";
+}
+
+function goreplit() {
+    window.location.href = "https://replit.com/~";
+}
+
+function gomiro() {
+    window.location.href = "https://miro.com/app/dashboard";
+}
